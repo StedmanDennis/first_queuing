c495e1dc4f323f949013b509b586268408586867
Stedman Dennis <stedmandennis@gmail.com>
2025-03-09 20:46:51 -0500

    fixed infinite rendering caused by dependency array in use-local-storage-hook

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,16 +1,16 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 function useLocalStorage<T>(key: string, initialValue: T) {
   const [value, setValue] = useState<T>(initialValue)
 
-  const updateValue = useCallback((value: T) => {
+  const updateValue = (value: T) => {
     if (typeof value === 'string'){
       localStorage.setItem(key, value)
     } else {
       localStorage.setItem(key, JSON.stringify(value))
     } 
     setValue(value)
-  }, [key])
+  }
 
   useEffect(() => {
     const browserValue = localStorage.getItem(key)
@@ -19,7 +19,8 @@ function useLocalStorage<T>(key: string, initialValue: T) {
     } else {
       updateValue(initialValue)
     } 
-  }, [key, initialValue, updateValue])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return [value, updateValue] as const;
 }
